58b999a16ab90d2d7a1f6e56b6f9a22f0e31371e
unknown <Andrew M>
2023-04-25 21:29:02 +0300

    the task is done

--- mainApp.java
+++ mainApp.java
@@ -4,10 +4,16 @@ public class mainApp {
     public static void main(String[] args) {
 
         company list = new company();
-        list.add("data1");
-        list.add("data2");
-
-        list.add(1, "data2_1");
+        list.add("private Ivanov");
+        list.add("private 1st class Smirnov");
+        list.add("sergeant Semenov");
+        list.add("sergeant 1st class Mirov");
+        list.add("liuetenant Gerasimov");
+        list.add(1, "private Ivanov");
+        list.add(2, "private 1st class Smirnov");
+        list.add(3, "sergeant Semenov");
+        list.add(4, "sergeant 1st class Mirov");
+        list.add(5, "liuetenant Gerasimov");
 
         Iterator<String> iterator = list.iterator();
         while (iterator.hasNext()) {
